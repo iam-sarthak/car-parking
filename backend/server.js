b923ae9a769c750c7fbfc9838011b78259611489
Sarthak Mittal <sarmittal@gmail.com>
2024-11-15 12:42:23 +0530

    bug removed

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use('/', (req, res)=>{
 
 // Connect to MongoDB and start server
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
+
+// Export the app for Vercel
+module.exports = app;
